6c405cce48792865810353b61ae962ceb93e44da
fatedane <tavani.lcs@gmail.com>
2023-03-20 11:47:45 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hello world</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          la taille du penis de corentin = {count} cm
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
