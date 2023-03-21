c0f1219026c255f5263b7ee43a47e9292fb666c3
fatedane <tavani.lcs@gmail.com>
2023-03-21 12:01:42 +0100

    login_lucas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import SignInSide from './sign-in-side/SignInSide'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SignInSide></SignInSide>
   </React.StrictMode>,
 )
