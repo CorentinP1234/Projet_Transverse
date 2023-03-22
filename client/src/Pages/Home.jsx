6bcbe38162b47290f4df4a0d6282183e8ad91e24
CorentinP1234 <corentin.prad@gmail.com>
2023-03-22 19:48:14 +0100

    first

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,4 +1,21 @@
+import React, {useEffect, useState} from 'react'
+
 export default function Home() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    // fetch("/api")
+    // fetch('http//localhost:5000/api')
+    fetch('test')
+    .then(
+      response => {response.json(); console.log(response)}
+    ).then(
+      data => {
+        // setBackendData(data)
+        console.log(data)
+      }
+    )
+  }, [])
 
   return (
     <div className="Container">
